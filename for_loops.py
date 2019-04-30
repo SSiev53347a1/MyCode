e1ec16cdecfde92afb15a9fd850f3833bed046ef
SSiev53347a1 <scott.l.sievers@verizon.com>
2019-04-30 14:07:48 -0400

    for loops

---
+++ for_loops.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# iterate over a range of numbers
+for i in range(10):
+    print(i)
+
+# itirate over a list
+animals = ["dog", "cat", "squirrel", "hamster"]
+
+for animal in animals:
+    print(animal)
+
+# iterate over a string
+rope = "braided"
+
+for x in range(len(rope)):
+     print(x)
+
+for x in rope:
+    print(x)
+
+
